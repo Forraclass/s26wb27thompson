b7eb47deec04a06bb76a50ff926369c71ef8d2ef
Forraclass <S547289@nwmissouri.edu>
2026-03-30 23:26:39 -0500

    Tristen, create QuixoticFruit page

--- routes/quixoticFruit.js
+++ routes/quixoticFruit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results: Quixotic Fruits' });
+  res.render('quixoticFruit', { title: 'Search Results: Quixotic Fruits' });
 });
 
 module.exports = router;
